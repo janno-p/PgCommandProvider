c3a1bcf60598ba940bd4760aea437c7f6fd84465
Janno Põldma <janno.poldma@gmail.com>
2014-10-06 10:20:14 +0300

    Initial type provider template. Testing PostgreSQL queries.

---
+++ PgCommandProvider/TestScript.fsx
@@ -0,0 +1,14 @@
+﻿#I @".\bin\Debug"
+
+#r "FSharp.Data.PgCommandProvider.dll"
+
+open FSharp.Data.Sql
+
+[<Literal>]
+let connectionString = "Server=127.0.0.1; Port=5432; Database=Jnx; User Id=Jnx; Password=Jnx;"
+
+type QueryAllCountries = PgCommand<"SELECT c1.*, (select count(*) from coins_coin) as x FROM coins_country c1", connectionString>
+
+let qry = QueryAllCountries()
+
+printfn "Country: {name:%s;code:%s;genitive:%s;id:%d;x:%d" qry.name qry.code qry.genitive qry.id qry.x
---
+++ PgCommandProvider/TypeProvider1.fs
@@ -0,0 +1,55 @@
+﻿namespace FSharp.Data.Sql
+
+open Npgsql
+open System.Reflection
+open Microsoft.FSharp.Core.CompilerServices
+open Samples.FSharp.ProvidedTypes
+
+[<TypeProvider>]
+type public PgCommandProvider() as this =
+    inherit TypeProviderForNamespaces()
+
+    let thisAssembly = Assembly.GetExecutingAssembly()
+    let rootNamespace = "FSharp.Data.Sql"
+    let baseTy = typeof<obj>
+    let staticParameters = [ProvidedStaticParameter("CommandText", typeof<string>)
+                            ProvidedStaticParameter("ConnectionString", typeof<string>)]
+
+    let pgCommandTy = ProvidedTypeDefinition(thisAssembly, rootNamespace, "PgCommand", Some baseTy)
+
+    do this.RegisterProbingFolder @"E:\Work\PgCommandProvider\packages\Npgsql.2.2.1\lib\net45"
+
+    do pgCommandTy.DefineStaticParameters(
+        staticParameters,
+        fun typeName args ->
+            match args with
+            | [| :? string as commandText; :? string as connectionString |] ->
+                use connection = new NpgsqlConnection(connectionString)
+                use command = new NpgsqlCommand(commandText, connection)
+                connection.Open()
+                use reader = command.ExecuteReader()
+
+                let properties = [ for i in 1..reader.FieldCount ->
+                                    ProvidedProperty(
+                                        reader.GetName(i - 1),
+                                        reader.GetProviderSpecificFieldType(i - 1),
+                                        GetterCode = fun args -> <@@ () @@>) ]
+
+                (*
+                let prop = ProvidedProperty(
+                            sprintf "WillGive%d" value,
+                            typeof<int>,
+                            GetterCode = fun args -> <@@ value @@>)
+                *)
+
+                let ty = ProvidedTypeDefinition(thisAssembly, rootNamespace, typeName, Some baseTy)
+                ty.AddMember(ProvidedConstructor([], InvokeCode = fun args -> <@@ () @@>))
+                ty.AddMembers(properties)
+                ty
+            | _ -> failwith "Unexpected parameter values"
+        )
+
+    do this.AddNamespace(rootNamespace, [pgCommandTy])
+
+[<TypeProviderAssembly>]
+do ()
