09c0b82bb672591fa5bbbfbf0424a40729c42fbc
Janno Põldma <janno.poldma@gmail.com>
2015-03-21 20:16:32 +0200

    Result properties.

--- PsqlCommandProvider/PsqlCommandProvider.fs
+++ PsqlCommandProvider/PsqlCommandProvider.fs
@@ -1,11 +1,16 @@
 ﻿namespace FSharp.Data.Psql
 
 open Microsoft.FSharp.Core.CompilerServices
+open Microsoft.FSharp.Quotations
 open Npgsql
 open ProviderImplementation.ProvidedTypes
 open System.Reflection
 open System.Text
 open System.Text.RegularExpressions
+
+type Entity() =
+    let data = System.Collections.Generic.Dictionary<string,obj>()
+    member __.GetValue(key) = match data.TryGetValue key with true, value -> value | _ -> null
 
 [<TypeProvider>]
 type PsqlCommandProvider(config: TypeProviderConfig) as this =
@@ -32,7 +37,7 @@ type PsqlCommandProvider(config: TypeProviderConfig) as this =
 
         let prepareName = "qry" + System.Guid.NewGuid().ToString().Replace("-", "").ToLower()
 
-        let regex = Regex(@"@\w+", RegexOptions.Compiled ||| RegexOptions.Multiline)
+        let regex = Regex(@":\w+", RegexOptions.Compiled ||| RegexOptions.Multiline)
 
         let parameters = [ for m in regex.Matches(sql) -> m.Value ] |> Set.ofList |> List.ofSeq
         let prepareQuery =
@@ -58,11 +63,35 @@ type PsqlCommandProvider(config: TypeProviderConfig) as this =
 
         let matchType = function
             | "text" -> typeof<string>
+            | "integer" -> typeof<int32>
             | x -> failwithf "Unknown type %s" x
 
-        let mp = (parameters, pty) ||> List.mapi2 (fun i p pt -> ProvidedParameter(p.Replace("@", ""), matchType pt))
+        let mp = (parameters, pty) ||> List.mapi2 (fun i p pt -> ProvidedParameter(p.Replace(":", ""), matchType pt))
+
+        use transaction = connection.BeginTransaction()
+
+        use action = new NpgsqlCommand(sql, connection, transaction)
+        parameters |> List.iter (fun s -> action.Parameters.Add(NpgsqlParameter(s.Replace(":", ""), null)) |> ignore)
+
+        let resultType =
+            use reader = action.ExecuteReader()
+            match reader.FieldCount with
+            | 0 ->
+                typeof<unit>
+            | n ->
+                let resultType = ProvidedTypeDefinition("Result", Some typeof<obj>, HideObjectMethods=true)
+                for i in 0 .. n - 1 do
+                    let name = reader.GetName(i)
+                    let typ = reader.GetFieldType(i)
+                    let prop = ProvidedProperty(name, typ)
+                    prop.GetterCode <- (fun args -> Expr.Coerce(<@@ ((%%args.[0]: obj) :?> Entity).GetValue(name) @@>, typ))
+                    resultType.AddMember(prop)
+                commandType.AddMember(resultType)
+                resultType :> System.Type
+
+        transaction.Rollback()
 
         commandType.AddMember(ProvidedConstructor([], InvokeCode = (fun _ -> <@@ () @@>)))
-        commandType.AddMember(ProvidedMethod("Execute", mp, typeof<unit>, InvokeCode = (fun _ -> <@@ () @@>)))
+        commandType.AddMember(ProvidedMethod("Execute", mp, resultType, InvokeCode = (fun _ -> <@@ Entity() @@>)))
 
         commandType
